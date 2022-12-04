452d58ff747b5a047c411fee08531b8183d21483
Shax <isama001@br.ru>
2022-12-05 00:30:50 +0300

    Создание локального поста

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app">
+        <form>
+            <h4>Создание поста</h4>
+            <input v-bind:value="title" @input="inputTitle" type="text" placeholder="Название">
+            <input v-bind:value="body" class="input" type="text" placeholder="Описанние">
+            <button class="btn" @click="createPost">Создать</button>
+        </form>
+    </div>
+    <div class="post" v-for="post in post" v-bind:key="post.id">
+        <div><strong>Название:</strong>{{post.title}}</div>
+        <div><strong>Описание:</strong>{{post.body}}</div>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data (){
+        return {
+           post: [
+            {id: 1, title: 'Javacript', body: 'Описание поста'},
+            {id: 2, title: 'Javacript2', body: 'Описание поста2'},
+            {id: 3, title: 'Javacript3', body: 'Описание поста3'},  
+            {id: 4, title: 'Javacript4', body: 'Описание поста4'}, 
+        ],
+        title: "",
+        body: ""
+        }
+    },
+    methods: {
+        createPost(){
+        },
+        inputTitle(event){
+console.log(event);
+        }
+    }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.post {
+    padding: 15px;
+    border: 2px solid rgb(94, 2, 255);
+    margin-top: 5px;
+}
+.input{
+    width: 100%;
+    border:1px solid rgb(94, 2, 255);
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+.app {
+    padding: 20px;
+}
+form {
+    display: flex;
+    flex-direction: column;
+}
+.btn{
+    padding: 10px 15px;
+    margin-top: 15px;
+    align-self: flex-end;
+    background: none;
+    color: rgb(94, 2, 255);
 }
 </style>
